c3694b0bbe128f069f91d553c2efcd84c9c445f8
Gabriellocp <gabriel_campista@hotmail.com>
2025-02-05 17:23:01 -0300

    chore: Add functions on router

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,11 @@ import multer from 'multer';
 import path from 'node:path';
 import { createCategory } from "./app/useCases/categories/createCategory";
 import { listCategories } from "./app/useCases/categories/listCategories";
+import { listProductsByCategory } from "./app/useCases/categories/listProductsByCaterory";
+import { changeOrderStatus } from "./app/useCases/orders/changeOrderStatus";
+import { createOrder } from "./app/useCases/orders/createOrder";
+import { deleteOrder } from "./app/useCases/orders/deleteOrder";
+import { listOrders } from "./app/useCases/orders/listOrders";
 import { createProduct } from "./app/useCases/products/createProducts";
 import { listProducts } from "./app/useCases/products/listProducts";
 export const router = Router()
@@ -35,31 +40,21 @@ router.post('/products', upload.single('image'), createProduct)
 
 // Get products by category
 
-router.get('/categories/:categoryId/products', (req, res) => {
-  res.send('OK')
-})
+router.get('/categories/:categoryId/products', listProductsByCategory)
 
 // List orders
 
-router.get('/orders', (req, res) => {
-  res.send('OK')
-})
+router.get('/orders', listOrders)
 
 // Create order
 
-router.post('/orders', (req, res) => {
-  res.send('OK')
-})
+router.post('/orders', createOrder)
 
 // Change order status
 
-router.patch('/orders/:orderId', (req, res) => {
-  res.send('OK')
-})
+router.patch('/orders/:orderId', changeOrderStatus)
 
 
 // Delete order
 
-router.delete('/orders/:orderId', (req, res) => {
-  res.send('OK')
-})
+router.delete('/orders/:orderId', deleteOrder)
